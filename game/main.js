90d4ec32fb97886604e35434194d81bf9d347fec
Regina Arslanova <aregina@yandex.ru>
2017-12-09 11:12:19 +0500

    score 200

--- game/main.js
+++ game/main.js
@@ -98,7 +98,7 @@ var mainState = {
     },
 
     checkScore: function() {
-        if (score >= 5) {
+        if (score >= 200) {
             var win_screen = game.add.sprite(game.world.centerX, game.world.centerY, 'win_screen');
             win_screen.anchor.setTo(0.5, 0.5);
             game.paused = true;
